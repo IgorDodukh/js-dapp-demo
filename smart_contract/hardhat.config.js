bc41d97a17e238fbef1bf7a089f5bccdc936798e
Ihor Dodukh <ihor.dodukh@gmail.com>
2022-09-09 00:50:54 +0100

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/X5ItyfjNRANBtSGpi5UtL_A953W8_-v8',
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/{ALCHEMY_API_KEY}',
       accounts: ['de09ea52190f26eb442eb51d0d42da070e34f24a4187a4b572db72b4599cad80']
     }
   }
